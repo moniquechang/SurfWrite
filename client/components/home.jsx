ad6b9d58df980a7f91d325771ed8397b1aef36d3
Monique Chang <moniquechang1997@gmail.com>
2022-09-15 18:27:14 +0000

    #1 edited layout to account for mobile and desktop screens

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -12,34 +12,49 @@ export default class Home extends React.Component {
   render() {
     return (
      <div className='container'>
-      <div className='row-1'>
-        <div className='col card mb-3'>
+      <h2 className='mb-5 mt-3'>This week&apos;s waves</h2>
+      <div className='row row-cols-md-4'>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Sunday:</div>
           <div className='card-body'></div>
         </div>
-        <div className='col card mb-3'>
+        </div>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Monday:</div>
           <div className='card-body'></div>
         </div>
-        <div className='col card mb-3'>
+        </div>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Tuesday:</div>
           <div className='card-body'></div>
         </div>
-        <div className='col card mb-3'>
+        </div>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Wednesday:</div>
           <div className='card-body'></div>
         </div>
-        <div className='col card mb-3'>
+        </div>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Thursday:</div>
           <div className='card-body'></div>
         </div>
-        <div className='col card mb-3'>
+        </div>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Friday:</div>
           <div className='card-body'></div>
         </div>
-        <div className='col card mb-3'>
+        </div>
+        <div>
+        <div className='col-11 col-md-auto card mb-5'>
           <div className='card-header'>Saturday:</div>
           <div className='card-body'></div>
+        </div>
         </div>
       </div>
     </div>
