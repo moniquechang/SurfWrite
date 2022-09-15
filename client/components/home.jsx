4284278b8f4f6b001052c00cfdac04c90b41914b
Monique Chang <moniquechang1997@gmail.com>
2022-09-15 21:53:21 +0000

    #1 added the current week dates to each day card

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -9,6 +9,23 @@ export default class Home extends React.Component {
     };
   }
 
+  getWeeklyDates(index) {
+    const currentDate = new Date();
+    const weekArr = [];
+    const todayDate = currentDate.getDate();
+    const todayDay = currentDate.getDay();
+
+    for (let i = 0; i < 7; i++) {
+      const day = new Date(currentDate);
+      day.setDate(todayDate + i - todayDay);
+      weekArr.push(day);
+    }
+
+    const weekDateString = weekArr.map(day => day.toLocaleDateString());
+    return weekDateString[index];
+
+  }
+
   render() {
     return (
      <div className='container'>
@@ -16,43 +33,43 @@ export default class Home extends React.Component {
       <div className='row row-cols-md-4'>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Sunday:</div>
+          <div className='card-header'>{`Sunday: ${this.getWeeklyDates(0)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Monday:</div>
+              <div className='card-header'>{`Monday: ${this.getWeeklyDates(1)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Tuesday:</div>
+              <div className='card-header'>{`Tuesday: ${this.getWeeklyDates(2)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Wednesday:</div>
+              <div className='card-header'>{`Wednesday: ${this.getWeeklyDates(3)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Thursday:</div>
+              <div className='card-header'>{`Thursday: ${this.getWeeklyDates(4)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Friday:</div>
+              <div className='card-header'>{`Friday: ${this.getWeeklyDates(5)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
         <div>
         <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>Saturday:</div>
+              <div className='card-header'>{`Saturday: ${this.getWeeklyDates(6)}`}</div>
           <div className='card-body'></div>
         </div>
         </div>
