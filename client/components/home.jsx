b80ddbeae1b862e58d42c0c8e6ac11b3f25e78e2
Monique Chang <moniquechang1997@gmail.com>
2022-09-15 23:55:39 +0000

    #1 added another method to 'home' component to create each day card instead of creating each card in the render method

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -26,55 +26,25 @@ export default class Home extends React.Component {
 
   }
 
+  createDayCards() {
+    const dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayCards = dayArr.map(day =>
+        <div key={day}>
+          <div className='col-11 col-md-auto card mb-5'>
+            <div className='card-header'>{`${day}: ${this.getWeeklyDates(dayArr.indexOf(day))}`}</div>
+            <div className='card-body'></div>
+          </div>
+        </div>
+    );
+    return dayCards;
+  }
+
   render() {
     return (
-     <div className='container'>
-      <h2 className='mb-5 mt-3'>This week&apos;s waves</h2>
-      <div className='row row-cols-md-4'>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-          <div className='card-header'>{`Sunday: ${this.getWeeklyDates(0)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-              <div className='card-header'>{`Monday: ${this.getWeeklyDates(1)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-              <div className='card-header'>{`Tuesday: ${this.getWeeklyDates(2)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-              <div className='card-header'>{`Wednesday: ${this.getWeeklyDates(3)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-              <div className='card-header'>{`Thursday: ${this.getWeeklyDates(4)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-              <div className='card-header'>{`Friday: ${this.getWeeklyDates(5)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
-        <div>
-        <div className='col-11 col-md-auto card mb-5'>
-              <div className='card-header'>{`Saturday: ${this.getWeeklyDates(6)}`}</div>
-          <div className='card-body'></div>
-        </div>
-        </div>
+      <div className='container'>
+        <h2 className='mb-5 mt-3'>This week&apos;s waves</h2>
+        <div className='row row-cols-md-4'>{this.createDayCards()}</div>
       </div>
-    </div>
     );
   }
 }
