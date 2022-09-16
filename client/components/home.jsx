d5ce609d2dc4bc2f23dfe920f4c93888f5f2b783
Monique Chang <moniquechang1997@gmail.com>
2022-09-16 20:32:01 +0000

    #1 added onClick event listener to send a fetch request to weather api for weather data for each specific day

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -8,9 +8,21 @@ export default class Home extends React.Component {
       locationLatitude: null,
       isClicked: false
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.getLocalDateString = this.getLocalDateString.bind(this);
+    this.getIsoDates = this.getIsoDates.bind(this);
   }
 
-  getWeeklyDates(index) {
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        locationLongitude: position.coords.longitude,
+        locationLatitude: position.coords.latitude
+      });
+    });
+  }
+
+  getWeeklyDates() {
     const currentDate = new Date();
     const weekArr = [];
     const todayDate = currentDate.getDate();
@@ -22,19 +34,32 @@ export default class Home extends React.Component {
       weekArr.push(day);
     }
 
-    const weekDateString = weekArr.map(day => day.toLocaleDateString());
-    return weekDateString[index];
+    return weekArr;
+  }
 
+  getLocalDateString(index) {
+    const weekDates = this.getWeeklyDates();
+    const localDateString = weekDates.map(day => day.toLocaleDateString());
+
+    return localDateString[index];
+  }
+
+  getIsoDates(index) {
+    const weekDates = this.getWeeklyDates();
+    const IsoString = weekDates.map(day => day.toISOString());
+    const finalDates = IsoString.map(day => day.substring(0, 10));
+
+    return finalDates[index];
   }
 
   createDayCards() {
     const dayArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const dayCards = dayArr.map(day =>
-        <div key={day}>
+    const dayCards = dayArr.map((day, index) =>
+        <div key={index}>
           <div className='col-11 col-md-auto card mb-5'>
-            <div className='card-header'>{`${day}: ${this.getWeeklyDates(dayArr.indexOf(day))}`}</div>
+            <div className='card-header'>{`${day}: ${this.getLocalDateString(dayArr.indexOf(day))}`}</div>
             <div className='card-body text-center p-0'>
-              <a className='weather-link mt-5'>
+              <a className='weather-link mt-5' onClick={this.handleClick} id={index}>
                 <i className="fa-solid fa-cloud mt-5"></i>
                 Check the Weather
                 <i className="fa-solid fa-cloud mt-5"></i>
@@ -43,16 +68,19 @@ export default class Home extends React.Component {
           </div>
         </div>
     );
+
     return dayCards;
   }
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.setState({
-        locationLongitude: position.coords.longitude,
-        locationLatitude: position.coords.latitude
-      });
-    });
+  handleClick(event) {
+    const startDate = this.getIsoDates(event.target.id);
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${this.state.locationLatitude}&longitude=${this.state.locationLongitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max,windgusts_10m_max&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto&start_date=${startDate}&end_date=${startDate}`)
+      .then(response => response.json())
+      .then(result => this.modalInfo(result));
+  }
+
+  modalInfo(data) {
+
   }
 
   render() {
