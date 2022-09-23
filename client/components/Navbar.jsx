e7ef5b910666c8ffc4527f999e6f01bcd0462054
Monique Chang <moniquechang1997@gmail.com>
2022-09-23 08:43:49 +0000

    #6 created app drawer with 'home' and 'past log' links

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
+
   render() {
     return (
-      <nav className='navbar'>
-        <div className='container'>
-          <a className='app-name'>SurfWrite</a>
-          <a><i className="fa-solid fa-bars"></i></a>
+      <>
+        <nav className='navbar'>
+          <div className='container'>
+            <a className='app-name'>SurfWrite</a>
+            <a><i className="fa-solid fa-bars"></i></a>
+          </div>
+        </nav>
+        <div className='modal-background-menu'>
+          <div className='menu-window'>
+            <div className='menu-anchor-div'>
+              <a className='menu-anchor'>Home</a>
+            </div>
+            <div className='menu-anchor-div'>
+              <a className='menu-anchor'>Past Logs</a>
+            </div>
+          </div>
         </div>
-      </nav>
+      </>
     );
   }
 }
