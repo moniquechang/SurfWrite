1b9a1c51e1f3bcd9595e10e472e5a7633c61d813
Monique Chang <moniquechang1997@gmail.com>
2022-09-23 08:54:04 +0000

    #6 added onClick event handler to open and close app drawer when menu icon is clicked

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+
+    this.handleClickMenu = this.handleClickMenu.bind(this);
+  }
+
+  handleClickMenu() {
+    if (this.state.isClicked) {
+      this.setState({ isClicked: false });
+    } else {
+      this.setState({ isClicked: true });
+    }
+  }
 
   render() {
+    let menuModalBackground;
+    if (this.state.isClicked) {
+      menuModalBackground = 'modal-background-menu';
+    } else {
+      menuModalBackground = 'modal-background-menu hidden';
+    }
     return (
       <>
         <nav className='navbar'>
           <div className='container'>
             <a className='app-name'>SurfWrite</a>
-            <a><i className="fa-solid fa-bars"></i></a>
+            <a><i className="fa-solid fa-bars" onClick={this.handleClickMenu}></i></a>
           </div>
         </nav>
-        <div className='modal-background-menu'>
+        <div className={menuModalBackground}>
           <div className='menu-window'>
             <div className='menu-anchor-div'>
               <a className='menu-anchor'>Home</a>
