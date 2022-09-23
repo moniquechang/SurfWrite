74edff3d0fe328449cfac7a22720c5d39043a686
Monique Chang <moniquechang1997@gmail.com>
2022-09-23 00:39:05 +0000

    #6 created navbar that includes app name and menu icon on Navbar component

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 export default class Navbar extends React.Component {
-
+  render() {
+    return (
+      <nav className='navbar'>
+        <div className='container'>
+          <a className='app-name'>SurfWrite</a>
+          <a><i className="fa-solid fa-bars"></i></a>
+        </div>
+      </nav>
+    );
+  }
 }
